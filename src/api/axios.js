ebbd0c16c75db61a5b18ba2c8b0a35eaf22b6d0d
ChetanXpro <chetanbaliyan10@gmail.com>
2022-12-17 22:47:31 +0530

    Feat: updated api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,14 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://user-api-urfj.onrender.com/";
 
 export default axios.create({
   baseURL: BASE_URL,
+  timeout:9000
 });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
+  timeout:9000,
   withCredentials: true,
 });
